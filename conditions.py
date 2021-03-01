e2b3d93848529263e8b9f7c9f90b8741b655306e
Mand09 <79094554+Mand09@users.noreply.github.com>
2021-03-01 04:31:38 -0500

    find out what to wear when you input a temperature

---
+++ conditions.py
@@ -0,0 +1,19 @@
+temp=0
+while temp!= 999:
+    temp=input("Please enter the temperature outside:")
+    if temp >= "90":
+        print("Wear shorts")
+    elif temp >="70":
+        print("Wear short sleeves")
+    elif temp >="50":
+        print("Bring a jacket")
+    elif temp >="32":
+        print("Wear a heavy jacket")
+    elif temp<"32" or temp>100:
+        print("Stay Inside")    
+    else:
+        break
+
+
+
+   
