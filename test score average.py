e23bf3f8b38aedaaaceec2120df97be21a80b846
Mand09 <79094554+Mand09@users.noreply.github.com>
2021-03-01 04:32:13 -0500

    Find the average of your testscores

--- test score average.py
+++ test score average.py
@@ -1,12 +1,17 @@
 howManyEntered = 0
-total = 0
+total=0
 average = 0
-#this line inputs how many test scores user wants average
-howMany = input ("How many test scores would you like to average?")
-print(howMany)
-while howManyEntered < howMany:
-    testScore=input("Enter test score")
-    total=testScore + total
-    howManyEntered=+1
-    average=total/howMany
-print(average)
+howMany = int (input ("How many test scores would you like to average?"))
+while (howManyEntered < howMany):
+    howManyEntered=howManyEntered + 1
+    testScores= input("Enter Test score " + str(howManyEntered)+ ": ")
+    testScore=float(testScores)
+    total= total+testScore
+
+average= total/howManyEntered    
+print("The average of you test scores is: " + str(average) +"%")   
+
+       
+
+
+
